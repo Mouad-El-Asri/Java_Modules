0cda5c52036c23ac86db51e5e571131e134a4848
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-11-23 20:52:11 +0100

    ex05: Add initial Program.java implementation with user input handling

---
+++ java_module_00/ex05/Program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		System.out.print("-> ");
+		final String input = scanner.nextLine();
+		System.out.println();
+		scanner.close();
+	}
+}
